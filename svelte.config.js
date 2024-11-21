c6532119329b35c612fddf523876e53a8e90307f
Mark Snyder <20092441+mkwsnyder@users.noreply.github.com>
2024-11-20 23:33:49 -0700

    add vite inspector

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
 	},
-
-	extensions: ['.svelte', '.svx']
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'meta-shift',
+		},
+	},
+	extensions: ['.svelte', '.svx'],
 };
 
 export default config;
