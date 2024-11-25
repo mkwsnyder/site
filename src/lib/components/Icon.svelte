e7913f2d8c2230ebefb18942ea7813dcbfa3d356
Mark Snyder <20092441+mkwsnyder@users.noreply.github.com>
2024-11-25 06:14:26 -0700

    add several icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,34 +1,42 @@
 <script lang="ts">
-  import {type Icon, icons} from "$lib/icon";
+	import { type Icon, icons } from '$lib/icon';
 
-  interface Props {
-    icon: Icon;
-    alt: string;
-    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-  }
+	interface Props {
+		icon: Icon;
+		alt: string;
+		size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+	}
 
-  const { icon: iconName, alt, size = 'md' }: Props = $props();
+	const { icon: iconName, alt, size = 'md', ...rest }: Props = $props();
 
-  const { viewBox, d } = icons[iconName];
+	const { viewBox, d: _d } = icons[iconName];
 
-  const rem = {
-    xs: 1,
-    sm: 1.5,
-    md: 2,
-    lg: 3,
-    xl: 4,
-  }[size];
-
+	const rem = {
+		xs: 1,
+		sm: 1.5,
+		md: 2,
+		lg: 3,
+		xl: 4,
+	}[size];
 </script>
 
 <!-- TODO: colors -->
-<svg xmlns="http://www.w3.org/2000/svg" height="{rem}rem" width="{rem}rem" {viewBox} fill="white">
-  <title>{alt}</title>
-  <path {d} />
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	height="{rem}rem"
+	width="{rem}rem"
+	{viewBox}
+	fill="white"
+	{...rest}
+>
+	<title>{alt}</title>
+	{#each _d as d}
+		<path {d} />
+	{/each}
 </svg>
 
 <style>
-  /*svg {*/
-  /*  stroke: white;*/
-  /*}*/
+	/*svg {*/
+	/*  stroke: white;*/
+	/*}*/
 </style>
