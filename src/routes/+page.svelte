11fda335a52e2067433fe657a6ef485e71b9d731
Rain Snyder <20092441+mkwsnyder@users.noreply.github.com>
2025-09-16 16:49:16 -0600

    update basic site info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <p>
 	Hello, and welcome to my (very) in progress portfolio/blog/everything site. As you can see, it's
 	currently lacking all the niceties you might expect from any decent site (e.g. responsive themes),
-	along with much information. I will be updating this over the next week or two to get this up to a
+	along with much information. I will be updating this over the next while to get this up to a
 	decent level of quality.
 </p>
 <p>Check back soon&trade; for a more "complete" version.</p>
