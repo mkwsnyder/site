017a8dd8f03e191af358b61401a4f6c006edc1a8
Rain Snyder <20092441+mkwsnyder@users.noreply.github.com>
2026-09-01 22:17:29 -0600

    update to fix autoplay restriction

--- src/routes/(misc)/ck/+page.svelte
+++ src/routes/(misc)/ck/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { pageTitle } from '$lib/utils';
+	import { fly } from 'svelte/transition';
+
+	let paused = $state(true);
+	let currentTime = $state(0);
+
+	function onclick() {
+		paused = !paused;
+		currentTime = 0;
+	}
 </script>
 
-<audio autoplay loop>
+<audio loop bind:paused bind:currentTime>
 	<source
 		src="https://6tfv44zxmv.ufs.sh/f/m2Vjr4arYXupllflOAgJqby62neDIXRl4Hgu1o5ZWKc9fiML"
 		type="audio/wav"
@@ -13,7 +22,16 @@
 	<title>{pageTitle('???')}</title>
 </svelte:head>
 
-<div class="text">[ headphones recommended ]</div>
+<!-- <div class="text"> -->
+{#if paused}
+	<button class="text" {onclick} transition:fly={{ y: -20, duration: 500 }}>[ click me ]</button>
+{:else}
+	<button class="text" {onclick} transition:fly={{ y: 20, duration: 500 }}>
+		[ headphones recommended ]
+	</button>
+{/if}
+
+<!-- </div> -->
 
 <style>
 	.text {
@@ -23,5 +41,12 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+	}
+
+	button {
+		background: none;
+		border: none;
+		padding: 1rem;
+		cursor: pointer;
 	}
 </style>
