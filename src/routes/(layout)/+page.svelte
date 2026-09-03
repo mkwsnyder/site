9ffc0fe6504e2f8d730b6bc9579641a1b4f9e74c
Rain Snyder <20092441+mkwsnyder@users.noreply.github.com>
2026-09-03 00:12:34 -0600

    update landing page

--- src/routes/(layout)/+page.svelte
+++ src/routes/(layout)/+page.svelte
@@ -4,8 +4,7 @@
 <p>
 	Hello, and welcome to my (very) in progress portfolio/blog/everything site. As you can see, it's
 	currently lacking all the niceties you might expect from any decent site (e.g. responsive themes),
-	along with much information. I will be updating this over the next while to get this up to a
+	along with most information. I will be updating this over the next while to get this up to a
 	decent level of quality.
 </p>
 <p>Check back soon&trade; for a more "complete" version.</p>
-<p><a href="/projects" data-sveltekit-preload-data>Projects</a></p>
